2f07b3229e07db95742a5ba42a25bae9ebc3d3db
DiegoFChacon <dyewoc@gmail.com>
2025-01-16 14:45:40 -0500

    cambio de hdv

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/cv.pdf';
+import CV from '../../assets/cvPy.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
@@ -69,7 +69,7 @@ function Hero() {
           passionate about technology, I try to learn more every day.
         </p>
         <a href={CV} download>
-          <button className="hover">Resume⬇️</button>
+          <button className="hover">Resume⏬</button>
         </a>
       </div>
     </section>
